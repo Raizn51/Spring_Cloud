f5c5f3be5e6b7a52320c7e04325b19221f1a8af0
Raizn51 <Gouravraikwar989@gmail.com>
2025-03-20 11:48:12 +0530

    [Gourav Raikwar] Add . Spring Boot security OAuth 2

---
+++ SecureApp/src/main/java/com/gourav_raikwar/secureapp/UserRepository.java
@@ -0,0 +1,8 @@
+package com.gourav_raikwar.secureapp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	User findByUsername(String username);
+
+}
